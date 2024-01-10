789e7e49c462f36d778fb0ca35b3906de5042043
Sunny-117 <zhiqiangfu6@gmail.com>
2024-01-10 13:39:52 +0800

    chore: release v0.0.1

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,6 +1,6 @@
 import { dirname, resolve } from "node:path"
 import { fileURLToPath } from "node:url"
-import { cherry } from 'cherry'
+import { cherry } from '@sunny-117/cherry'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
